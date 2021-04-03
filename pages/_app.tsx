074be6347781de276e3675edf3f39364d2ea7fd1
ravi-sanapala <69839322+ravi-sanapala@users.noreply.github.com>
2021-04-03 10:03:45 +0530

    Managed the ServerStyleSheets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 // https://nextjs.org/docs/advanced-features/custom-app
 
-import App from 'next/app';
+// import App from 'next/app';
+import {useEffect} from 'react'
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import {ThemeProvider} from '@material-ui/core/styles'
 import {themeDark,themeLight} from 'lib/theme';
 export default function MyApp({Component, pageProps}){
+
+     useEffect(() => {
+          //Remove the server side injected css
+          const jssStyles = document.querySelector('#jss-server-side')
+          if (jssStyles && jssStyles.parentNode){
+               jssStyles.parentNode.removeChild(jssStyles);
+          }
+     }, [])
      return( 
            <ThemeProvider theme={false ? themeDark: themeLight}>
                 <CssBaseline></CssBaseline>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,20 @@
 //https://nextjs.org/docs/advanced-features/custom-document
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import {ServerStyleSheets} from '@material-ui/core';
+import React from 'react'
 class MyDocument extends Document{
     static async getInitialProps(ctx) {
+         //Render the app and get the context of the page with collected side effects
+         const sheets = new ServerStyleSheets();
+         const orginalRenderPage = ctx.renderPage;
+         ctx.renderPage = () => orginalRenderPage({enhanceApp:(App) => (props) => sheets.collect(<App {...props}/>) })
+
+
+
+
         const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps }
+        return { ...initialProps,styles:[...React.Children.toArray(initialProps.styles), sheets.getStyleElement()] }
     }
     render(){
         return(
