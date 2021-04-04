b4210489ea7f4dd81fafcfe3f34943e80ff517de
ravi-sanapala <69839322+ravi-sanapala@users.noreply.github.com>
2021-04-04 12:35:30 +0530

    Update object-id-scalar.ts

--- api/schema/object-id-scalar.ts
+++ api/schema/object-id-scalar.ts
@@ -1,2 +1,21 @@
 import { GraphQLScalarType, Kind} from 'graphql';
 import { ObjectId} from 'mongodb';
+
+export const ObjectIdScalar = new GraphQLScalarType({
+    name:"ObjectId",
+    description:"Mongod id scalar type",
+    parseValue(value:string){
+        return new ObjectId(value); // Client input variable
+    },
+    serialize(value:ObjectId){
+        return value.toHexString() // Value sent to client
+    },
+    parseLiteral(ast){
+             if(ast.kind === Kind.STRING){
+                 return new ObjectId(ast.value) // value from the client query 
+             }
+             return null;
+    }
+
+    
+})
